fdee75d27ac447716040208fa9936dd37eaa8f08
Skinsella <kinsella.sj@gmail.com>
2019-08-29 18:01:58 -0500

    Latest changes, not final product yet

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,23 +14,32 @@ with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     header = next(csvreader)
-    months = 0
-    
+    months = []
+    net_pl = []
+    change_pl = []
         # Loop through the data
     for row in csvreader:
       
 #The total number of months included in the dataset
         
-        months += 1
+        months.append(row[0])
 
 #The net total amount of "Profit/Losses" over the entire period
-        
+        net_pl.append(float(row[1]))
 
 #The average of the changes in "Profit/Losses" over the entire period
-
+    for i in range(1,len(net_pl)):
+        change_pl.append(net_pl[i] - net_pl[i-1])
+        average_pl = sum(change_pl) / len(change_pl)
 #The greatest increase in profits (date and amount) over the entire period
-
+        high_profit = max(change_pl)
+        high_month = str(months[change_pl.index(max(change_pl))])
 #The greatest decrease in losses (date and amount) over the entire period
+        low_proft = min(change_pl)
+        low_month = str(months[change_pl.index(min(change_pl))])
 
-
-print (f"Total Months: {months}")
+print (f"Total Months: {len(months)}")
+print (f"Total: {sum(net_pl)}")
+print (f"Average Change: {round(average_pl,2)}")
+print (high_profit, high_month)
+print (low_proft, low_month)
